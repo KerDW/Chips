8a82d0f11df09305b57810b236448149dccf8e93
Álvaro Zamora <alvarozajim@gmail.com>
2020-02-18 20:48:00 +0100

    item class

---
+++ item.py
@@ -0,0 +1,25 @@
+class Item:
+    itemCount = 0
+
+    def __init__(self, pos, name):
+        self.pos = pos
+        self.name = name
+
+    @property
+    def name(self):
+        return self.name
+
+    @name.setter
+    def name(self, name):
+        self.name = name
+
+    @property
+    def pos(self):
+        return self.pos
+
+    @pos.setter
+    def pos(self, pos):
+        self.pos = pos
+
+    def displayCount(self):
+        return Item.itemCount
