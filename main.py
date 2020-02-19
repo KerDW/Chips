9f725588166c2d83224a164a47d8900df3e1700f
Álvaro Zamora <alvarozajim@gmail.com>
2020-02-19 19:58:52 +0100

    remove colors

--- main.py
+++ main.py
@@ -9,14 +9,6 @@ WIDTH = 576
 HEIGHT = 576
 FPS = 30
 
-# define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
