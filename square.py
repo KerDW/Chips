d91cc1389b948572ffb1540d6133c6c9125cedd2
Álvaro Zamora <alvarozajim@gmail.com>
2020-02-19 11:05:33 +0100

    square class changes
    square type integers clarified, if else chain to load the proper image and some getters and setters

--- square.py
+++ square.py
@@ -1,4 +1,6 @@
 import pygame
+
+
 class Square:
     squareCount = 0
 
@@ -7,20 +9,35 @@ class Square:
     water_square = 'sprites/water_square.png'
     ice_square = 'sprites/ice_square.png'
     fire_square = 'sprites/fire_square.png'
-    s_type = None
-    img = None
+
+    NORMAL_SQUARE = 0
+    WALL_SQUARE = 1
+    WATER_SQUARE = 2
+    ICE_SQUARE = 3
+    FIRE_SQUARE = 4
 
 
-    def __init__(self, item, sprite, stype):
-        #self.item = item
-        self.sprite = sprite
-        self.s_type = stype
+    def __init__(self, item, sprite_type):
+        self._item = item
+        self._sprite_type = sprite_type
 
-        if(self.s_type == 0):
-            self.img = pygame.image.load(self.normal_square)
+        # Changing the image or the sprite type should execute this code to change the image I think
+
+        if self._sprite_type == NORMAL_SQUARE:
+            self._image = pygame.image.load(self.normal_square)
+
+        elif self._sprite_type == WALL_SQUARE:
+            self._image = pygame.image.load(self.wall_square)
+
+        elif self._sprite_type == WATER_SQUARE:
+            self._image = pygame.image.load(self.water_square)
+
+        elif self._sprite_type == ICE_SQUARE:
+            self._image = pygame.image.load(self.ice_square)
+
+        elif self._sprite_type == FIRE_SQUARE:
+            self._image = pygame.image.load(self.fire_square)
         
-        else:
-            self.img = pygame.image.load(self.wall_square)
 
     @property
     def item(self):
@@ -28,13 +45,16 @@ class Square:
 
     @item.setter
     def item(self, item):
-        self.item = item
+        self._item = item
+
+    @property
+    def sprite_type(self):
+        return self._sprite_type
+
+    @sprite.setter
+    def sprite_type(self, sprite_type):
+        self._sprite_type = sprite_type
 
     def displayCount(self):
         return Square.chipCount
 
-    def getImg(self):
-        return self.img
-
-    def hasItem(self):
-        return self.item
