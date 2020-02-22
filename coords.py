2b240e9658366f58735e3cc7074d45fd1e164997
Álvaro Zamora <alvarozajim@gmail.com>
2020-02-22 12:23:18 +0100

    coordinates class

---
+++ coords.py
@@ -0,0 +1,26 @@
+class Coords:
+
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        self._x = x
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, y):
+        self._y = y
+
+    def toArray(self):
+        return [self._x, self._y]
+
+
