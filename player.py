993938d2d8babf31f8288431df4b9dbcdc95b74b
Álvaro Zamora <alvarozajim@gmail.com>
2020-04-02 17:56:38 +0200

    changed player and enemy coords to rect and checking collision working
    move_ip is just a way to directly set the rect of something to a x and y

--- player.py
+++ player.py
@@ -1,14 +1,16 @@
 import pygame
 
+from coords import Coords
 
 class Player(pygame.sprite.Sprite):
 
     def __init__(self, gameMap):
         super().__init__()
         self._image = pygame.image.load('sprites/player.png').convert_alpha()
+        self._rect = self._image.get_rect()
+        
         self._items = []
         self._chips = []
-        self._coords = None
         self._gameMap = gameMap
         self._score = 0
         self._username = None
@@ -17,27 +19,41 @@ class Player(pygame.sprite.Sprite):
         screen.blit(self._image, coords.toArray())
 
     def drawAtCurrentCoords(self, screen):
-        screen.blit(self._image, self._coords.toArray())
+        screen.blit(self._image, self._rect)
 
     def moveUp(self):
-        if self._gameMap.canMoveThere(self._coords.x, self._coords.y - 64, self):
-            self._coords.y -= 64
+        if self._gameMap.canMoveThere(self._rect.x, self._rect.y - 64, self):
+            self._rect.y -= 64
 
     def moveDown(self):
-        if self._gameMap.canMoveThere(self._coords.x, self._coords.y + 64, self):
-            self._coords.y += 64
+        if self._gameMap.canMoveThere(self._rect.x, self._rect.y + 64, self):
+            self._rect.y += 64
 
     def moveLeft(self):
-        if self._gameMap.canMoveThere(self._coords.x - 64, self._coords.y, self):
-            self._coords.x -= 64
+        if self._gameMap.canMoveThere(self._rect.x - 64, self._rect.y, self):
+            self._rect.x -= 64
 
     def moveRight(self):
-        if self._gameMap.canMoveThere(self._coords.x + 64, self._coords.y, self):
-            self._coords.x += 64
+        if self._gameMap.canMoveThere(self._rect.x + 64, self._rect.y, self):
+            self._rect.x += 64
+            
+    def collidedWithEnemy(self, enemy):
+        if self._rect.colliderect(enemy.rect):
+            print("player collided with enemy")
+            return 1
+        return 0
 
     @property
     def image(self):
         return self._image
+    
+    @property
+    def rect(self):
+        return self._rect
+    
+    @rect.setter
+    def rect(self, rect):
+        self._rect = rect
 
     @image.setter
     def image(self, image):
@@ -50,15 +66,7 @@ class Player(pygame.sprite.Sprite):
     @property
     def chips(self):
         return self._chips
-
-    @property
-    def coords(self):
-        return self._coords
-
-    @coords.setter
-    def coords(self, coords):
-        self._coords = coords
-        
+    
     @property
     def username(self):
         return self._username
